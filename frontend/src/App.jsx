ea9c7e1e589a86c107a7bd14d96620f7e996d1e0
Dawid <153355666+NapBon2003@users.noreply.github.com>
2025-11-26 19:33:06 +0100

    Added primitive edit and add movie site. Added new movies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 // frontend/src/App.jsx
 import { useEffect, useState } from "react";
 import searchIcon from './assets/search.svg';
+import logo from './assets/logo/logo.svg';
 import VideoPosterTile from "./VideoPoster";
 
 function App() {
   const [items, setItems] = useState([]);
   const [title, setTitle] = useState("");
   const [page, setPage] = useState("home");
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const movieTiles = [
+    { number: 0, title: "ADD NEW MOVIE", page: "add" },
+    { number: 1, title: "The Fast and The Furious: Tokyo Drift", page: "edit" },
+    { number: 2, title: "Matrix", page: "edit" },
+    { number: 3, title: "Se7en", page: "edit" },
+    { number: 4, title: "Indiana Jones and Raiders of the Lost Ark", page: "edit" },
+    { number: 5, title: "Beverly Hills Cop II", page: "edit" },
+    { number: 6, title: "Pulp Fiction", page: "edit" },
+    { number: 7, title: "Minecraft Movie", page: "edit" },
+    { number: 8, title: "Kiler", page: "edit" },
+    { number: 9, title: "Znachor", page: "edit" },
+    { number: 10, title: "The Pianist", page: "edit" },
+  ];
+
 
   useEffect(() => {
     fetch("http://localhost:8000/items")
@@ -33,44 +49,162 @@ function App() {
   };
 
   return (
-  <div className="mainbody">
-    {page === "home" && (
-      <>
-        <div className="header" style={{ fontFamily: "sans-serif"}}>
-          <div className="search_bar">
-            <form onSubmit={handleSubmit} style={{ marginBottom: "1.5rem" }}>
-              <input
-                type="text"
-                placeholder="Search..."
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <button type="submit">
-                <img src={searchIcon} alt="Search" width="40" height="40" />
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div className="main">
-          <div className="main_container">
-            <div className="movie_container">
-              <VideoPosterTile number={0} title="ADD NEW MOVIE" onClick={() => setPage("add")} />
-              <VideoPosterTile number={1} title="The Fast and The Furious: Tokyo Drift" onClick={() => setPage("edit")} />
-              <VideoPosterTile number={2} title="Matrix" onClick={() => setPage("edit")} />
-              <VideoPosterTile number={3} title="Se7en" onClick={() => setPage("edit")} />
-              <VideoPosterTile number={4} title="Indiana Jones" onClick={() => setPage("edit")} />
-              <VideoPosterTile number={5} title="Beverly Hills Cop II" onClick={() => setPage("edit")} />
+    <div className="mainbody">
+      {page === "home" && (
+        <>
+          <div className="header" style={{ fontFamily: "sans-serif" }}>
+            <div className="logo"><img src={logo} alt="logo" /></div>
+            <div className="search_bar">
+              <form onSubmit={handleSubmit} style={{ marginBottom: "1.5rem" }}>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <button type="submit">
+                  <img src={searchIcon} alt="Search" width="40" height="40" />
+                </button>
+              </form>
             </div>
           </div>
-        </div>
-      </>
-    )}
 
-    {page === "edit" && <h1>MOVIE EDITOR</h1>}
-    {page === "add" && <h1>ADD MOVIE</h1>}
-  </div>
-);
+          <div className="main">
+            <div className="main_container">
+              <div className="movie_container">
+                {movieTiles.map(({ number, title, page: targetPage }) => (<VideoPosterTile key={number} number={number} title={title} onClick={() => {
+                  setSelectedMovie({ number, title, page: targetPage }); setPage(targetPage);
+                }} />))}
+              </div>
+            </div>
+          </div>
+          <div className="footer">
+            <p>Credits: Dawid, Michał, Filip<br />2025</p>
+          </div>
+        </>
+      )}
+
+      {page === "edit" && selectedMovie && (
+        <>
+          <div className="header" style={{ fontFamily: "sans-serif" }}>
+            <div className="logo" onClick={() => setPage("home")}><img src={logo} alt="logo" /></div>
+          </div>
+          <div className="main">
+            <div className="editor">
+              <div className="editor-left">
+                <VideoPosterTile key={selectedMovie.number} number={selectedMovie.number} title={selectedMovie.title} onClick={() => {
+                  setSelectedMovie(selectedMovie);
+                  setPage("edit");
+                }} />
+              </div>
+              <div className="editor-right">
+                <h2>Edit data for movie:</h2>
+                <form onSubmit={handleSubmit} style={{ marginBottom: "1.5rem" }}>
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Title</p>
+                    <p>Title of the movie</p>
+                    <input type="text" placeholder="TITLE" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Year</p>
+                    <p>Year of movie premiere</p>
+                    <input type="text" placeholder="YEAR" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Genre</p>
+                    <p>Movie category or type (e.g. Action, Drama)</p>
+                    <input type="text" placeholder="GENRE" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Description</p>
+                    <p>Short summary of the movie plot</p>
+                    <input type="text" placeholder="DESCRIPTION" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Director</p>
+                    <p>Name of the movie director</p>
+                    <input type="text" placeholder="DIRECTOR" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Review</p>
+                    <p>Social opinion or rating of the movie</p>
+                    <input type="text" placeholder="REVIEW" />
+                  </div>
+
+                  <button type="submit">
+                    <img src={searchIcon} alt="Search" width="40" height="40" />
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {page === "add" && selectedMovie && (
+        <>
+          <div className="header" style={{ fontFamily: "sans-serif" }}>
+            <div className="logo" onClick={() => setPage("home")}><img src={logo} alt="logo" /></div>
+          </div>
+          <div className="main">
+            <div className="editor">
+              <div className="editor-left">
+                <VideoPosterTile key={selectedMovie.number} number={selectedMovie.number} title={selectedMovie.title} onClick={() => {
+                  setSelectedMovie(selectedMovie);
+                  setPage("edit");
+                }} />
+              </div>
+              <div className="editor-right">
+                <h2>Add data for movie:</h2>
+                <form onSubmit={handleSubmit} style={{ marginBottom: "1.5rem" }}>
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Title</p>
+                    <p>Title of the movie</p>
+                    <input type="text" placeholder="TITLE" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Year</p>
+                    <p>Year of movie premiere</p>
+                    <input type="text" placeholder="YEAR" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Genre</p>
+                    <p>Movie category or type (e.g. Action, Drama)</p>
+                    <input type="text" placeholder="GENRE" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Description</p>
+                    <p>Short summary of the movie plot</p>
+                    <input type="text" placeholder="DESCRIPTION" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Director</p>
+                    <p>Name of the movie director</p>
+                    <input type="text" placeholder="DIRECTOR" />
+                  </div>
+
+                  <div>
+                    <p style={{ fontSize: "2em",fontWeight: "bold" }}>Review</p>
+                    <p>Social opinion or rating of the movie</p>
+                    <input type="text" placeholder="REVIEW" />
+                  </div>
+                  <button type="submit"></button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 
 }
 
