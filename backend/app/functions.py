e32b6ad5b1f3c2170efb10d3a7a0a9c237471434
JockerCard <szablowski.zero@gmail.com>
2025-12-16 13:26:16 +0100

    move variables ALGORITHM and SECRET_KEY to .env

--- backend/app/functions.py
+++ backend/app/functions.py
@@ -7,6 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
 from sqlalchemy.orm import Session
+import os
 
 from .crud import get_user
 from .schemas import TokenData
@@ -29,9 +30,8 @@ def validate_user(db: Session,username: str,passw: str):
         return False
     return True
 
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
